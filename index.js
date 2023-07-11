b417f3ac09fce2359a131cf90d40eefa67b3ce85
npallav <nav.pallav@mercedes-benz.com>
2023-07-11 18:52:49 +0530

    Changes when connected to vercel.

--- index.js
+++ index.js
@@ -114,8 +114,13 @@ app.delete("/nav/surveyApp/delete", (req, res, next) => {
  * @description To make the server listen at port 5000
  */
 const port = process.env.PORT || 5050;
-app.listen(port, () => {
-  console.log(`Survey Server started as ${process.env.PORT}|| 5050`);
+app.listen(port, (err, res) => {
+  if (err) {
+      console.log(err)
+      return res.status(500).send(err.message);
+  } else {
+      console.log('[INFO] Server Running on port:', port)
+  }
 });
 
 module.exports = app;
