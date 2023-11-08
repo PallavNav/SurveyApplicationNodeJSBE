4c03ead669338fbef60913d1cdfadaabe81c60e0
npallav <nav.pallav@mercedes-benz.com>
2023-11-08 16:04:30 +0530

    With mailer code added.

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
+const nodeMailer = require("nodemailer");
 const SurveyData = require("./models/questionSchema");
+const { Credentials } = require("./service.js");
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
-const databaseURL =
- `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzdr4vx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const databaseURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzdr4vx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const mailOptions = {
+  from: "nav.pallav@mercedes-benz.com",
+  to: ["nav.pallav@mercedes-benz.com", "pallavnav@gmail.com"],
+  subject: "How are you doing? Just stay there!I cannot be prefectionist!! ",
+  text: "This is a test email sent from a Node.js app!",
+};
 
 /**
  * @description - Request header to be passed to every API.
@@ -21,7 +29,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
@@ -62,9 +70,9 @@ const handleValidation = (request) => {
   return errorCode;
 };
 
-app.get('/nav/surveyApp/aboutSurveyApp', (req, res) => {
-  res.send('Hey this is my API running 🥳');
-})
+app.get("/nav/surveyApp/aboutSurveyApp", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
 /**
  * @description To handle the api request, only official API
@@ -109,10 +117,10 @@ app.delete("/nav/surveyApp/delete", (req, res, next) => {
     });
 });
 
-/*** 
+/***
  * @description To fetch all the survey data entered!
-*/
-app.get('/nav/surveyApp/fetchAllData', (req, res, next) => {
+ */
+app.get("/nav/surveyApp/fetchAllData", (req, res, next) => {
   SurveyData.find()
     .then((response) => {
       res.status(200).json({ submittedSurveyData: response });
@@ -124,15 +132,48 @@ app.get('/nav/surveyApp/fetchAllData', (req, res, next) => {
 
 const removeDuplicate = (data) => {
   const uniqueResponse = new Set();
-  return data.filter((obj)=> {
-    if(!uniqueResponse.has(obj.submittedBy)) {
+  return data.filter((obj) => {
+    if (!uniqueResponse.has(obj.submittedBy)) {
       uniqueResponse.add(obj.submittedBy);
       return true;
     }
     return false;
-  })
-}
+  });
+};
 
+const manageMailSender = () => {
+  const mailer = nodeMailer.createTransport({
+    service: Credentials().gmail.source,
+    auth: Credentials().gmail.auth,
+  });
+  return mailer;
+};
+
+const sendMail = (mailerDetails, mailOptions) => {
+  return new Promise((resolve, reject) => {
+    mailerDetails.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        reject(error);
+      } else {
+        console.log("Mail sent successfully!!");
+        resolve(info);
+      }
+    });
+  });
+};
+
+app.get("/nav/surveyApp/sendMail", async (req, res, next) => {
+  try {
+    const senderDetails = manageMailSender(); // Configure sender details
+    const mailOptions1 = mailOptions;
+    const response = await sendMail(senderDetails, mailOptions1); // Send the email
+
+    res.status(200).json({ submittedSurveyData: response });
+  } catch (error) {
+    res.status(500).json({ error: "Error sending email" });
+  }
+});
 
 /***
  * @description To make the server listen at port 5000
@@ -140,10 +181,10 @@ const removeDuplicate = (data) => {
 const port = process.env.PORT || 4050;
 app.listen(port, (err, res) => {
   if (err) {
-      console.log(err)
-      return res.status(500).send(err.message);
+    console.log(err);
+    return res.status(500).send(err.message);
   } else {
-      console.log('[INFO] Server Running on port:', port)
+    console.log("[INFO] Server Running on port:", port);
   }
 });
 
---
+++ service.js
@@ -0,0 +1,18 @@
+module.exports.Credentials = () => {
+  return {
+    gmail: {
+      source:'Gmail',  
+      auth: {
+        user: "pallavnav@gmail.com",
+        pass: "ooce shso gsav uuhb",
+      },
+    },
+    outlook: {
+      source:'Outlook',
+      auth: {
+        user: "nav.pallav@mercedes-benz.com",
+        pass: "Vijaya124@t",
+      },
+    },
+  };
+};
